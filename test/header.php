7427f587918630e00eb6bf2185ad4726710a3d60
Felix-aigner <33003840+Felix-aigner@users.noreply.github.com>
2018-03-09 19:22:02 +0100

    Test-Versuch und anfang vom eigentlichen projekt

---
+++ test/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Page Title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="res/vongStyle.css"
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="nav-wrapper">
+                    <ul>
+                        <li><a href"index.php">Home</a></li>
+                        
+                    </ul>
+                    <div class="nav-login">
+                        <form >
+                            <input type="text" name='username' placeholder="Username"/><br/>
+                            <input type="password" name='password' placeholder="Password"/><br/>
+                            <button type="submit" name="submit">Login</button>
+                        </form>
+                        <a href="signup.php">Sign up</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
