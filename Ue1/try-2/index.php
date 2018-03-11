d6a4f69b000093b30428e59bb85f25bc91025c6f
Felix-aigner <33003840+Felix-aigner@users.noreply.github.com>
2018-03-11 18:41:55 +0100

    abd

--- Ue1/try-2/index.php
+++ Ue1/try-2/index.php
@@ -1,22 +1,44 @@
 <?php
+    session_start();
+    $user = array(
+        "user" => "test",
+        "pass"=>"1234"          
+    );
     $error = "";
-    if(isset($_POST['username'],$_POST['password'])){
- 
-        
-        $user = array(
-                "user" => "test",
-                "pass"=>"1234"          
-                );
+    
+    if(!isset($_COOKIE['']))
+    
+    
+
+    //echo daten
+    echo session_id()."<br/>";
+    echo $_COOKIE['user']."<br/>";
+    echo $cookie_page."<br/>";
+    //echo daten
+
+    
+    if(isset($_POST['username'],$_POST['password']))
+    {
         $username = $_POST['username'];
         $pass = $_POST['password'];
-        if($username == $user['user'] && $pass == $user['pass']){
-            session_start();
+        if($username == $user['user'] && $pass == $user['pass'])
+        {
+            if(!isset($_COOKIE['user']))
+            {
+                setcookie("user", $username, time() + (86400 * 30), "/"); // 86400 = 1 day
+            }
             $_SESSION['simple_login'] = $username;
-            include("home.php");
-            exit();
-        }else{
-            $error = '<div class="alert alert-danger">Invalid Login</div>';
         }
+        else
+        {
+            $error = '<div class="alert alert-danger">Invalid Login</div>';   
+        }
+
+    }
+
+    if($_COOKIE['user']==$user['user'] && !isset($_SESSION['simple-login']))
+    {
+        $_SESSION['simple_login']=$_COOKIE['user'];
     }
 ?>
 <!DOCTYPE html>
@@ -26,36 +48,35 @@
     <title>WT-Webshop</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" type="text/css" rel="stylesheet">
-    <link href="style.css" type="text/css" rel="stylesheet">
+    <link href="res/style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-    <nav>
-        <div class="login-form">
-            <h3>Please sign in</h3>
-            <?php echo $error; ?>
-            <form method="post" action="index.php">
-                <fieldset>
-                    <div class="form-group">
-                        <input placeholder="Username" name="username" type="text">
-                    </div>
-                    <div class="form-group">
-                        <input placeholder="Password" name="password" type="password" value="">
-                    </div>
-                        <input type="submit" value="Login"/>
-                </fieldset>
-            </form>
+
+    <header>
+        <div class="container">
+            <img src="res/banner-shop.jpg" alt="Banner-Shop"/>
         </div>
+    </header>
+    <?php 
+    if(!isset($_SESSION['simple_login']))
+    {
+        include("login-form.php"); 
+    }
+    else
+    {
+        include("logout-form.php");
+    }
+    ?>
 
-    </nav>
-    <main>
-
-        <h2>Welcome to WT-Webshop</h2>
-        <h3>To view further information login</h3>
-
-
-    </main>
-
-
+    <?php
+        if(isset($_GET['section']))
+        {
+            setcookie("page", $_SESSION['section'], time() + (86400 * 30), "/");
+            $page = $_SESSION['section'];
+            echo "<br/><h2 style=\"text-align: center;\"> $page </h2>";
+        }
+        
+    ?>
 
     </body>
 </html>
--- Ue1/try-2/logout.php
+++ Ue1/try-2/logout.php
@@ -1,5 +1,8 @@
 <?php 
     session_start();
+    unset($_COOKIE['user']);
+    unset($_COOKIE['page']);
+
     unset($_SESSION['simple_login']);
     header("Location: index.php");
 ?>
