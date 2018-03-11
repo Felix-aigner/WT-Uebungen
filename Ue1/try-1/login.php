22709a8f928db3c783b381fa791e227a003bc0e1
Felix-aigner <33003840+Felix-aigner@users.noreply.github.com>
2018-03-11 18:44:50 +0100

    all at once

---
+++ Ue1/try-1/login.php
@@ -0,0 +1,8 @@
+<nav>
+   Hello Anonymous!<br/>
+    <form action="validate-login.php" method="post">
+        <input type="text" name="user" /><br/>
+        <input type="password" name="password" /><br/>
+        <input type="submit" value="Einloggen" />
+    </form>
+</nav>
---
+++ Ue1/try-1/main-loggedin.php
@@ -0,0 +1,5 @@
+
+<main>
+    <h1>You are sucessfully logged in</h1>
+    do shit you stupid php thingy
+</main>
